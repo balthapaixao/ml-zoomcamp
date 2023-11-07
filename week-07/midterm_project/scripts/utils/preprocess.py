3e765969c8e5cd6b3f2ef32c888b7154148565d2
balthapaixao <balthazarpaixao@gmail.com>
2023-11-07 17:45:14 -0300

    feat: developing the model

---
+++ week-07/midterm_project/scripts/utils/preprocess.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+
+def create_flight_code():
+    ...
+
+
+def classify_stops():
+    ...
+
+
+def classify_time_of_day():
+    ...
+
+
+def classify_day_of_week():
+    ...
+
+
+def classify_holiday():
+    ...
+
+
+def days_until_flight():
+    ...
+
+
+def create_flight_duration():
+    ...
+
+
+def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+    ...
