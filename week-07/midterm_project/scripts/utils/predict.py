20cd1de53235cb250d519ab1d604ea144dfc76ce
balthapaixao <balthazarpaixao@gmail.com>
2023-11-08 14:41:37 -0300

    predict improvements

--- week-07/midterm_project/scripts/utils/predict.py
+++ week-07/midterm_project/scripts/utils/predict.py
@@ -14,3 +14,22 @@ def read_model():
     with open("../../data/models/model.bin", "rb") as f:
         model, dv, scaler = pickle.load(f)
     return model, dv, scaler
+
+
+def transform_input(df, dv, scaler):
+    df = df.copy()
+    df = df[
+        [
+            "flight_code",
+            "stops",
+            "time_of_day",
+            "day_of_week",
+            "holiday",
+            "days_until_flight",
+            "flight_duration",
+        ]
+    ]
+    df_dict = df.to_dict(orient="records")
+    X = dv.transform(df_dict)
+    X = scaler.transform(X)
+    return X
